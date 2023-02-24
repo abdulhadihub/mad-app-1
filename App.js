607db17e86476967b667766d6610218563bf9858
Abdul Hadi <88116803+LocTrain@users.noreply.github.com>
2023-02-24 16:01:47 +0500

    created sign Up page

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import { Button } from 'react-native-web';
+// import styles from 'app.css'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.logoView}>
+
+        <Image
+          style={styles.tinyLogo}
+          source={require('./assets/logo.png')}
+        />
+      </View>
+      <View style={styles.textfieldsView}>
+        <Text style={styles.userNameLabel}>Please Enter your User Name:</Text>
+        <TextInput style={styles.usernameField} placeholder='Username' />
+
+        <Text style={styles.userNameLabel}>Please Enter your Password:</Text>
+        <TextInput style={styles.usernameField} placeholder='Password' />
+
+      </View>
+
+      <View style={styles.buttonsView}>
+        <Button style={styles.signUpBtn} title='Sign Up' />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +36,40 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    borderColor: 'black',
+    borderWidth: 1,
   },
+  tinyLogo: {
+    width: 70,
+    height: 70,
+  },
+  logoView: {
+    flex: 0.2,
+    padding: 10,
+    justifyContent: 'center',
+    // backgroundColor: 'red',
+  },
+  textfieldsView: {
+    flex: 0.5,
+    justifyContent: 'center',
+    // backgroundColor: 'blue',
+  },
+  usernameField: {
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 4,
+  },
+  userNameLabel: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    padding: 10,
+  },
+  buttonsView: {
+    flex: 0.3,
+    // backgroundColor: 'green',
+  },
+  signUpBtn: {
+    borderRadius: 100,
+
+  }
 });
